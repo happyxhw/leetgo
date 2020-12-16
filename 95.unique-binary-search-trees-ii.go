e722a39bcf457239b55dd390743df18af90e3209
happyxhw <happyxhw@outlook.com>
2020-12-16 20:57:26 +0800

    init

---
+++ 95.unique-binary-search-trees-ii.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=95 lang=golang
+ *
+ * [95] Unique Binary Search Trees II
+ */
+package leetgo
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+	var res []*TreeNode
+
+	return res
+}
+
+func helper(n int, res []*TreeNode) []*TreeNode {
+
+}
+
+// @lc code=end
